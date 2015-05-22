4d4ae1f074b7177be8c8b6642c7458940a3dbf5f
forevermatt <forevermatt@users.noreply.github.com>
2015-05-22 15:08:21 -0500

    Make done page more intuitive (info message, clearer buttons).

--- protected/views/learn/done.php
+++ protected/views/learn/done.php
@@ -5,7 +5,7 @@
 $this->pageTitle = 'Done';
 ?>
 
-<div class="alert alert-success" role="alert">
+<div class="alert alert-info" role="alert">
     <b>You're done!</b> That was the last example we have for the question
     "<?php echo CHtml::encode($question->text); ?>".
 </div>
@@ -14,12 +14,12 @@ $this->pageTitle = 'Done';
     echo sprintf(
         'Now you can either %s or %s',
         sprintf(
-            '<a href="%s">%s</a>',
+            '<a href="%s" class="btn btn-default">%s</a>',
             $this->createUrl('learn/'),
             'go back to the list of questions'
         ),
         sprintf(
-            '<a href="%s">%s</a>',
+            '<a href="%s" class="btn btn-default">%s</a>',
             $this->createUrl('learn/question', array(
                 'slug' => $question->urlSlug,
             )),
